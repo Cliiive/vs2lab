44987dc065ea5458b95009f69ab63a34a4b95588
rb807 <r.c.babcock@icloud.com>
2025-11-30 21:05:54 +0100

    splitter v1

---
+++ lab3/wordcount/splitter.py
@@ -0,0 +1,22 @@
+import const
+import time
+import zmq
+
+NUM_MAPPERS = 3
+
+
+context = zmq.Context()
+sender = context.socket(zmq.PUSH)  # create a push socket
+
+address = "tcp://" + const.HOST + ":" + const.SPLITTER_PORT  # how and where to communicate
+sender.bind(address)  # bind socket to the address
+
+print(f"[SPLITTER] Running at {address}")
+
+time.sleep(1) # wait to allow all clients to connect
+
+with open('text.txt', 'r') as file:
+    contents = file.readlines() # read contents of a text file
+
+for line in contents:
+    sender.send(line.encode())
