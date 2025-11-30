576b060906aff0fdab4d44461d89991967190d64
Edge Runner <74921222+ArtemKabin@users.noreply.github.com>
2025-11-30 19:42:02 +0000

    update const (use only const no args)

--- lab3/wordcount/const.py
+++ lab3/wordcount/const.py
@@ -1,4 +1,16 @@
 HOST = "127.0.0.1"
 SPLITTER_PORT = "50007"
-MAPPER_PORT = "50008"
-REDUCER_PORT = "50009"
+MAPPER_PORT = "500042"
+REDUCER_PORT = "50074"
+
+DONE = "9_9_9";
+# List of words to count (case-insensitive)
+WORDS_TO_COUNT = [
+    "the",
+    "and",
+]
+
+# Defines numbers of components to start
+NUM_SPLITTERS = 1
+NUM_MAPPERS = 3
+NUM_REDUCERS = 2
