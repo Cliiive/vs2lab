e568a70cd21e927cb9dff6c2eb2f3efddebd9608
rb807 <r.c.babcock@icloud.com>
2025-12-07 14:32:14 +0000

    logger for splitter

--- lab3/wordcount/splitter.py
+++ lab3/wordcount/splitter.py
@@ -3,6 +3,9 @@ import time
 import zmq
 import logging
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+logger = logging.getLogger("SPLITTER")
+
 def splitter():
     context = zmq.Context()
     sender = context.socket(zmq.PUSH)  # create a push socket
@@ -10,8 +13,7 @@ def splitter():
     address = "tcp://" + const.HOST + ":" + const.SPLITTER_PORT  # how and where to communicate
     sender.bind(address)  # bind socket to the address
 
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
-    logging.info(f"[SPLITTER] Running at {address}")  # important lifecycle
+    logger.info(f"Running at {address}")
 
     time.sleep(1) # wait to allow all clients to connect
 
@@ -21,8 +23,10 @@ def splitter():
     for line in contents:
         logging.debug("[SPLITTER] sending line")  # routine flow
         sender.send(line.encode())
+        logger.debug(f"Sent line: {line.strip()}")
 
     # Send one DONE per mapper to allow all mapper threads to terminate
+    logger.info("Sending DONE")
     for _ in range(const.NUM_MAPPERS):
         logging.debug("[SPLITTER] sending DONE")  # routine flow
         sender.send_string(const.DONE)
